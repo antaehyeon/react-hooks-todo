46b6a2296080a0ebe3e71edc075f2f68d612c0f3
HYEON(Mac) <antaehyeon@live.co.kr>
2019-03-10 00:56:27 +0900

    Feat: todo html & use Hooks method (useState)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
+import List from "./List";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [inputText, setInputText] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const handleTodoInputText = e => {
+    console.log("todoInputTextHandler e-value", e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos, inputText]);
+  };
+
+  return (
+    <>
+      <h1>-TODO APPLICATIONS-</h1>
+      <form action="">
+        <input type="text" name="" onChange={handleTodoInputText} />
+        <button onClick={addTodo}>할일 추가</button>
+      </form>
+
+      <List todos={todos} />
+    </>
+  );
+};
 
 export default App;
